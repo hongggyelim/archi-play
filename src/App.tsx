75bb37b54aa99698e6c0d86cdf7c7324e8ddf2e5
hong <hongggy@gmail.com>
2026-02-23 21:57:20 +0900

    set planary map

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { RenderView } from "./components/render-view/render-view";
-import { List } from "./components/temp-list-component/list";
+import RenderView from "./components/render-view/render-view";
 
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<List />}></Route>
-          <Route path="/view" element={<RenderView />}></Route>
-          <Route path="*" element={<List />}></Route>
+          <Route path="/" element={<RenderView />}></Route>
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
